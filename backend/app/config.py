d2048248fc0030a4df8577af5c6ce0f1a7d57050
syiism <syiism@163.com>
2026-07-10 22:59:30 +0800

    build: add python-dotenv and env config files
    add python-dotenv dependency, create local .env file and example env config,
    refactor config to load env variables from .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,10 @@
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(env_path, override=True)
 
 DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
@@ -8,6 +14,6 @@ DB_NAME = os.getenv("DB_NAME", "bubble_community")
 
 UC_KEY = os.getenv("UC_KEY", "L6r4t3X9IcL5M2n657g5t626J4F404Xb02w4vao7Pfm1C0E0Uay4tcF00dDbRdK9")
 
-UC_LOGIN_URL = "https://vossc.com/member.php"
-UC_USER_URL = "https://vossc.com/home.php"
-UC_AVATAR_URL = "https://vossc.com/uc_server/avatar.php"
+UC_LOGIN_URL = os.getenv("UC_LOGIN_URL", "https://vossc.com/member.php")
+UC_USER_URL = os.getenv("UC_USER_URL", "https://vossc.com/home.php")
+UC_AVATAR_URL = os.getenv("UC_AVATAR_URL", "https://vossc.com/uc_server/avatar.php")
